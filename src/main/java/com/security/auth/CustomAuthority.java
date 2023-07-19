b1cbe2fc6714b0390fb5428e2c16ef8d992bc5f2
spacedustz <cyberspecterr@gmail.com>
2023-07-19 22:44:20 +0900

    권한 저장 방식 변경 (Inmemory -> DB)

---
+++ src/main/java/com/security/auth/CustomAuthority.java
@@ -0,0 +1,34 @@
+package com.security.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/* User의 권한을 Mapping, 생성하는 Custom Authority 클래스 */
+@Component
+public class CustomAuthority {
+
+    private String adminName = "admin";
+
+    // 관리자용 권한 리스트
+    private final List<String> ADMIN_ROLE = List.of("ADMIN", "USER");
+
+    // 일반 유저용 권한 리스트
+    private final List<String> USER_ROLES = List.of("USER");
+
+    // DP에 저장된 Role을 기반으로 권한 정보 생성
+    // SimpleGrantedAuthority에 Role을 매핑할때 "ROLE_" 을 안붙이면 매핑이 안됨
+    public List<GrantedAuthority> createAuthorities(List<String> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> createRoles(String name) {
+        if (name.equals(adminName)) return ADMIN_ROLE;
+        return USER_ROLES;
+    }
+}
