89b10f9369a2152960ef5e438878654e9c08e061
spacedustz <cyberspecterr@gmail.com>
2023-07-19 22:43:08 +0900

    Create JPQL Query

--- src/main/java/com/security/user/UserRepository.java
+++ src/main/java/com/security/user/UserRepository.java
@@ -1,6 +1,13 @@
 package com.security.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "select u from User u where u.name = :name")
+    Optional<User> findByName(@Param("name") String name);
 }
