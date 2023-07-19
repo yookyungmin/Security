542a6cc2efa95a524d3b63277a80ea153c8e363d
spacedustz <cyberspecterr@gmail.com>
2023-07-19 22:43:25 +0900

    Add Function

--- src/main/java/com/security/user/UserService.java
+++ src/main/java/com/security/user/UserService.java
@@ -1,16 +1,10 @@
 package com.security.user;
 
+import com.security.auth.CustomAuthority;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Transactional
 @RequiredArgsConstructor
@@ -19,10 +13,9 @@ public class UserService {
     private final UserDetailsManager manager;
     private final PasswordEncoder encoder;
     private final UserRepository userRepository;
+    private final CustomAuthority authority;
 
     public User create(UserDto.Post data) {
-        // User 권한 지정
-        List<GrantedAuthority> auth = createAuthorities(Role.ROLE_USER.name());
 
         // Password Encoding
         String encrypt = encoder.encode(data.getPassword());
@@ -30,19 +23,11 @@ public class UserService {
         // Create User
         User user = User.createOf(data.getName(), encrypt, data.getAge());
 
-        UserDetails details = (UserDetails) user;
-
-        manager.createUser(details);
+        // Role 생성
+        user.setRole(authority.createRoles(data.getName()));
 
         userRepository.save(user);
 
         return user;
-    }
-
-    // SimpleGrantedAuthority 사용할때 'ROLE_' + 권한명으로 지정 안해주면 권한 매핑 안됨
-    private List<GrantedAuthority> createAuthorities(String... roles) {
-        return Arrays.stream(roles)
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
     }
 }
