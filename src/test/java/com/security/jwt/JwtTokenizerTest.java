a8862c4dae6220d039a86295e3d354e799a301da
spacedustz <cyberspecterr@gmail.com>
2023-07-20 16:40:48 +0900

    Jwt Token Verify 함수 검증 테스트

--- src/test/java/com/security/jwt/JwtTokenizerTest.java
+++ src/test/java/com/security/jwt/JwtTokenizerTest.java
@@ -1,13 +1,14 @@
 package com.security.jwt;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.junit.jupiter.api.*;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /* Jwt Token 생성 테스트 */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -25,6 +26,7 @@ public class JwtTokenizerTest {
     }
 
     @Test
+    @DisplayName("Access Token 생성 테스트")
     public void generateAccessTokenTest() {
         // 토큰의 유저 정보
         Map<String, Object> claims = new HashMap<>();
@@ -50,6 +52,7 @@ public class JwtTokenizerTest {
     }
 
     @Test
+    @DisplayName("Refresh Token 생성 테스트")
     public void generateRefreshTokenTest() {
         String subject = "Test Refresh Token";
         Calendar calendar = Calendar.getInstance();
@@ -61,5 +64,37 @@ public class JwtTokenizerTest {
         System.out.println(refreshToken);
 
         assertThat(refreshToken, notNullValue());
+    }
+
+    @Test
+    @DisplayName("JWT Signature 검증 테스트")
+    public void verifySignatureTest() {
+        String accessToken = getAccessToken(Calendar.MINUTE, 10);
+        assertDoesNotThrow(() -> tokenizer.verifySignature(accessToken, base64EncodedSecretKey));
+    }
+
+    @Test
+    @DisplayName("Throw ExpiredJwtException When Jwt Verify")
+    public void verifyExpirationTest() throws InterruptedException {
+        String accessToken = getAccessToken(Calendar.SECOND, 1);
+        assertDoesNotThrow(() -> tokenizer.verifySignature(accessToken, base64EncodedSecretKey));
+
+        TimeUnit.MILLISECONDS.sleep(1500);
+
+        assertThrows(ExpiredJwtException.class, () -> tokenizer.verifySignature(accessToken, base64EncodedSecretKey));
+    }
+
+    // Access Token 생성 함수
+    private String getAccessToken(int timeUnit, int timeAmount) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", 1L);
+        claims.put("roles", List.of("USER"));
+
+        String subject = "test access token";
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(timeUnit, timeAmount);
+        Date expiration = calendar.getTime();
+
+        return tokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
     }
 }
