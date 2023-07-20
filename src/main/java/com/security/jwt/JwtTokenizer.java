2dc95eb29ef3373b8c4ce326e36c713f42f0ea24
spacedustz <cyberspecterr@gmail.com>
2023-07-20 16:24:17 +0900

    Jwt Tokenizer

---
+++ src/main/java/com/security/jwt/JwtTokenizer.java
@@ -0,0 +1,69 @@
+package com.security.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+public class JwtTokenizer {
+
+    // Plain Text 형태인 Secret Key의 byte[]를 Base64 형식의 문자열로 인코딩 해줌
+    public String encodeBase64SecretKey(String secretKey) {
+        return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * @param claims : 인증된 사용자 관련 정보
+     * @param subject : JWT Title
+     * @param expiration : Access Token 만료일
+     * @param base64EncodedSecretKey : Sign을 위한 Key를 만들기 위한 인코딩된 Secret Key
+     * @desc 인증된 사용자에게 JWT를 최초로 발급해주기 위한 JWT 생성 함수
+     */
+    public String generateAccessToken(
+            Map<String, Object> claims,
+            String subject,
+            Date expiration,
+            String base64EncodedSecretKey
+    ) {
+        Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
+
+        return Jwts.builder()
+                .setClaims(claims) // Authenticated User Info
+                .setSubject(subject) // JWT Tiele
+                .setIssuedAt(Calendar.getInstance().getTime()) // 발행일자 지정
+                .setExpiration(expiration) // 만료일 지정
+                .signWith(key) // 서명
+                .compact(); // JWT 생성, 직렬화
+    }
+
+    // Refresh Token 생성 함수, AccessToken 만료 시 재발급, 여기서 setClaims()는 필요 없다
+    public String generateRefreshToken(
+            String subject,
+            Date expiration,
+            String base64EncodedSecretKey
+    ) {
+        Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(Calendar.getInstance().getTime())
+                .setExpiration(expiration)
+                .signWith(key)
+                .compact();
+    }
+
+    // JWT의 Sign에 사용할 Secret Key를 생성해주는 함수
+    private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
+        // Encoding된 Secret Key를 Decoding 한 후, Byte Array 반환
+        byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
+
+        // Byte Array를 기반으로 적절한 HMAC 알고리즘을 적용한 Key 객체 반환
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
