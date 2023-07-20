02e25338d6f8f79cff46214453d7924a2ea57cee
spacedustz <cyberspecterr@gmail.com>
2023-07-20 23:50:34 +0900

    Sample Filter 주석처리

--- src/main/java/com/security/config/FilterConfig.java
+++ src/main/java/com/security/config/FilterConfig.java
@@ -1,27 +1,27 @@
-package com.security.config;
-
-import com.security.filter.FirstFilter;
-import com.security.filter.SecondFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/* Filter Chain 적용 Config */
-@Configuration
-public class FilterConfig {
-
-    // FilterRegistrationBean의 생성자로 Filter 인터페이스의 구현체를 넘겨주는 형태로 첫번째 필터 등록
-    @Bean
-    public FilterRegistrationBean<FirstFilter> registerFirstFilter() {
-        FilterRegistrationBean<FirstFilter> registrationBean = new FilterRegistrationBean<>(new FirstFilter());
-        registrationBean.setOrder(1); // 필터 우선순위 지정
-        return registrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean<SecondFilter> registerSecondFilter() {
-        FilterRegistrationBean<SecondFilter> registrationBean = new FilterRegistrationBean<>(new SecondFilter());
-        registrationBean.setOrder(2); // 필터 우선순위 지정
-        return registrationBean;
-    }
-}
+//package com.security.config;
+//
+//import com.security.filter.FirstFilter;
+//import com.security.filter.SecondFilter;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///* Filter Chain 적용 Config */
+//@Configuration
+//public class FilterConfig {
+//
+//    // FilterRegistrationBean의 생성자로 Filter 인터페이스의 구현체를 넘겨주는 형태로 첫번째 필터 등록
+//    @Bean
+//    public FilterRegistrationBean<FirstFilter> registerFirstFilter() {
+//        FilterRegistrationBean<FirstFilter> registrationBean = new FilterRegistrationBean<>(new FirstFilter());
+//        registrationBean.setOrder(1); // 필터 우선순위 지정
+//        return registrationBean;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean<SecondFilter> registerSecondFilter() {
+//        FilterRegistrationBean<SecondFilter> registrationBean = new FilterRegistrationBean<>(new SecondFilter());
+//        registrationBean.setOrder(2); // 필터 우선순위 지정
+//        return registrationBean;
+//    }
+//}
--- src/main/java/com/security/filter/SecondFilter.java
+++ src/main/java/com/security/filter/SecondFilter.java
@@ -1,27 +1,27 @@
-package com.security.filter;
-
-import jakarta.servlet.*;
-
-import java.io.IOException;
-
-public class SecondFilter implements Filter {
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-        System.out.println("Second Filter 생성");
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("===== Second Filter 시작 =====");
-        chain.doFilter(request, response);
-        System.out.println("===== Second Filter 종료 =====");
-    }
-
-    @Override
-    public void destroy() {
-        System.out.println("Second Filter Destroy");
-        Filter.super.destroy();
-    }
-}
+//package com.security.filter;
+//
+//import jakarta.servlet.*;
+//
+//import java.io.IOException;
+//
+//public class SecondFilter implements Filter {
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//        System.out.println("Second Filter 생성");
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        System.out.println("===== Second Filter 시작 =====");
+//        chain.doFilter(request, response);
+//        System.out.println("===== Second Filter 종료 =====");
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("Second Filter Destroy");
+//        Filter.super.destroy();
+//    }
+//}
